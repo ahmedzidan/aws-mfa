8def683b976adec51806f3cb8f43ce235893fd7c
zidan <ahmedzidan_2015@hotmail.com>
2020-03-16 14:15:07 +0800

    use awk and call the api to get the serial

--- aws-mfa.sh
+++ aws-mfa.sh
@@ -2,19 +2,17 @@
 
 # Default filename values
 MFA_SERIAL_FILE=`echo ${HOME}/.aws/.mfaserial`
+Temp_MFA_SERIAL_FILE=`echo ${HOME}/.aws/.mfaserial-temp`
 AWS_TOKEN_FILE=`echo ${HOME}/.aws/.awstoken`
 AWS_CREDENTIALS_PATH=`echo ${HOME}/.aws/credentials`
 DURATION_SECONDS=129600
 
 inputMFASerial() {
-echo "Tip - You can get your ARN for MFA device here: https://console.aws.amazon.com/iam/home#/security_credentials"
-while true; do
-    read -p "Please input your MFA ARN: " mfa
-    case $mfa in
-        "") echo "Please input a valid value.";;
-        * ) echo $mfa > $MFA_SERIAL_FILE; break;;
-    esac
-done
+   MFA_SERIAL_number=`aws iam list-mfa-devices --output text`;
+   echo "$MFA_SERIAL_number" > "$Temp_MFA_SERIAL_FILE";
+   echo "`awk '{print $3}' $Temp_MFA_SERIAL_FILE`" >> ${MFA_SERIAL_FILE}
+   rm -f $Temp_MFA_SERIAL_FILE
+   echo "your mfaserial has been saved"
 }
 
 
@@ -27,25 +25,27 @@ getTempCredential(){
       esac
   done
 
-  # Run the awscli command
-  # shellcheck disable=SC2006
-  authenticationOutput=`aws sts get-session-token --serial-number ${MFA_SERIAL} --token-code ${MFA_TOKEN} --duration-seconds ${DURATION_SECONDS}`
+  authenticationOutput=`aws sts get-session-token --serial-number ${MFA_SERIAL} --token-code ${MFA_TOKEN} --duration-seconds ${DURATION_SECONDS} --output text`
 
-  # Save authentication to some file
-  echo "$authenticationOutput" > "$AWS_TOKEN_FILE";
-  storeTempCredential
+  if [ ! -z "$authenticationOutput" ]; then
+        # Save authentication to some file
+        echo "$authenticationOutput" > "$AWS_TOKEN_FILE";
+        storeTempCredential
+        echo '[mfa] profle has been updated! Please add --profile mfa to aws commands you want to run. Eg: aws s3 ls --profile mfa'
+  fi
+
 }
 
 storeTempCredential() {
 
  perl -0777 -i -pe 's/\n+\[mfa\]\naws_access_key_id = [[:upper:][:digit:]]+\naws_secret_access_key = [[:alnum:]+\/]+\naws_session_token = [[:alnum:]+\/]+\n?//igs' ${AWS_CREDENTIALS_PATH}
 
- echo "
+echo "
 
 [mfa]
-aws_access_key_id = `echo ${authenticationOutput} | jq -r '.Credentials.AccessKeyId'`
-aws_secret_access_key = `echo ${authenticationOutput} | jq -r '.Credentials.SecretAccessKey'`
-aws_session_token = `echo ${authenticationOutput} | jq -r '.Credentials.SessionToken'` " >> ${AWS_CREDENTIALS_PATH}
+aws_access_key_id = `awk '{print $2}' $AWS_TOKEN_FILE `
+aws_secret_access_key = `awk '{print $4}' $AWS_TOKEN_FILE`
+aws_session_token = `awk '{print $5}' $AWS_TOKEN_FILE` " >> ${AWS_CREDENTIALS_PATH}
 }
 
 
@@ -59,15 +59,15 @@ MFA_SERIAL=`cat $MFA_SERIAL_FILE`
 
 if [ -e $AWS_TOKEN_FILE ]; then
   authenticationOutput=`cat $AWS_TOKEN_FILE`
-  authExpiration=`echo $authenticationOutput | jq -r '.Credentials.Expiration'`
+  authExpiration=`awk '{print $3}' $AWS_TOKEN_FILE`
   nowTime=`date -u +'%Y-%m-%dT%H:%M:%SZ'`
 
   if [ "$authExpiration" \< "$nowTime" ]; then
     echo "Your last token has expired"
     getTempCredential
+  else
+    echo '[mfa] not expired yet! Please add --profile mfa to aws commands you want to run. Eg: aws s3 ls --profile mfa'
   fi
 else
   getTempCredential
 fi
-
-echo '[mfa] profile updated! Please add --profile mfa to aws commands you want to run. Eg: aws s3 ls --profile mfa'
